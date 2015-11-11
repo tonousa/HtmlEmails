3c48c396aa980a82c69b855d76b102dbdc968b34
tonousa <tonousa@hotmail.com>
2015-11-11 10:40:12 -0500

    1

---
+++ HtmlEmails/WebForm.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="HtmlEmails.WebForm" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="UserNameLbl" runat="server" Text="User Name"></asp:Label><br />
+        <asp:TextBox ID="UserName" runat="server"></asp:TextBox><br />
+        <asp:Button ID="Submit" runat="server" Text="Submit" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ HtmlEmails/WebForm.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace HtmlEmails
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                //UserName.Text = "submitted";
+                string newFileBody = PopulateBody(UserName.Text, "", "", "");
+                using (StreamWriter writer = 
+                    new StreamWriter(Server.MapPath("EmailOutput.htm"), false))
+                {
+                    writer.Write(newFileBody);
+                }
+                Response.Redirect("EmailOutput.htm");
+            }
+        }
+
+        private string PopulateBody(string userName, string title, string url, string description)
+        {
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(Server.MapPath("~/EmailTemplate.htm")))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("{UserName}", userName);
+            //body = body.Replace("{Title}", title);
+            //body = body.Replace("{Url}", url);
+            //body = body.Replace("{Description}", description);
+            return body;
+        }
+    }
+}
